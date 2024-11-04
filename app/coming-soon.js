0cbf438221bee79c0b0adbb99f6b7275322092ae
ContiAugusto <acontiabreu987@gmail.com>
2024-11-04 07:54:27 -0300

    Adicionando o coming soon

---
+++ app/coming-soon.js
@@ -0,0 +1,23 @@
+const countdown = () => {
+    const targetDate = new Date("November 31, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = targetDate - now;
+  
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+  
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  };
+  
+  setInterval(countdown, 1000);
+  
